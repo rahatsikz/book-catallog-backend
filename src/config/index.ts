39479616ee6104cc57e09fd3281849cc3b1b2d94
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2024-06-02 21:18:48 +0600

    [done] ~ auth middleware

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,7 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   port: process.env.PORT,
   env: process.env.NODE_ENV,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
 };
